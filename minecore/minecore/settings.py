6bbc463ac97702793c25bfbaaf5972f0c93fd4f8
paritosh666 <paritosh.ghimire666@gmail.com>
2019-05-27 09:52:52 +0545

    Resolvinf psql exception : no auth table

--- minecore/minecore/settings.py
+++ minecore/minecore/settings.py
@@ -25,8 +25,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest_framework',
     'coreuseraccounts.apps.CoreuseraccountsConfig',
+    'rest_framework',
+
 
 ]
 
